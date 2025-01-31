d2082c64be18ace9f5b47d940f10f28ae631d8de
Alice <ikgmtm0714@outlook.jp>
2025-01-31 23:55:20 +0900

    Change name

---
+++ math.h
@@ -0,0 +1,10 @@
+
+
+typedef struct
+{
+    double deviation=0;//偏差値
+    double median=0;//中央値
+    double average=0;//平均値
+    double decentralization=0;//分散
+    double standard_deviation=0;//標準偏差
+}math;
