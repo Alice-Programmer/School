a6c31c976a833730770e3479f3f4e73880772986
Alice <ikgmtm0714@outlook.jp>
2025-02-01 00:02:23 +0900

    include Math.h and testing

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 #include <math.h>
 #include <vector>
+
+#include "Math.h"
 int main(void){
-    std::cout<<"Hello World";
+    math num;
+    num.average=0;
+
+    std::cout<<num.average;
     return 0;
 }
